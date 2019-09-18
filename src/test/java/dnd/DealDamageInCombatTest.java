a00688a7438cad5a2d465ffa657080632f9c6127
Marek <Marek.Myjak1221@gmail.com>
2019-09-18 21:34:45 +0200

    add Combat tests

---
+++ src/test/java/dnd/DealDamageInCombatTest.java
@@ -0,0 +1,39 @@
+package dnd;
+
+import dnd.character_information.Character;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class DealDamageInCombatTest {
+
+    @Mock
+    Character attacker;
+    @Mock
+    Character enemy;
+
+    @Test
+    void dealDamageOnHitTest() {
+        Mockito.when(attacker.attack(Mockito.any(Character.class), Mockito.anyInt())).thenReturn(AttackType.HIT);
+//        when
+        boolean attack = Combat.attack(attacker, enemy, 16);
+//        then
+        assertThat(attack).isEqualTo(true);
+        Mockito.verify(enemy, Mockito.times(1)).takeDamage(1);
+    }
+
+    @Test
+    void dealDoubleDamageOnCritTest() {
+        Mockito.when(attacker.attack(Mockito.any(Character.class), Mockito.anyInt())).thenReturn(AttackType.CRIT);
+//        when
+        boolean attack = Combat.attack(attacker, enemy, 16);
+//        then
+        assertThat(attack).isEqualTo(true);
+        Mockito.verify(enemy, Mockito.times(1)).takeDamage(2);
+    }
+}
